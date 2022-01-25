332835ce49be0df8ade7646ca7850a97f9d6eebf
OlegRo87 <oleg.rohlin@gmail.com>
2022-01-25 10:54:37 +0200

    24.1

---
+++ quiz_game/data.py
@@ -0,0 +1,45 @@
+question_data = [
+    {
+
+        "question": "Manchester United won the 2013-14 English Premier League.",
+        "correct_answer": "False",
+    },
+    {
+
+        "question": "Skateboarding will be included in the 2020 Summer Olympics in Tokyo.",
+        "correct_answer": "True",
+    },
+    {
+        "question": "Tennis was once known as Racquetball.",
+        "correct_answer": "False",
+    },
+    {
+        "question": "The Olympics tennis court is a giant green screen.",
+        "correct_answer": "True",
+    },
+    {
+        "question": "Roger Federer is a famous soccer player.",
+        "correct_answer": "False",
+    },
+    {
+        "question": "In Rugby League, performing a &quot;40-20&quot; is punished by a free kick for the opposing team.",
+        "correct_answer": "False",
+    },
+    {
+        "question": "During Wimbledon, spectators in the grounds can buy the tennis balls that have been used in "
+                    "matches.",
+        "correct_answer": "True",
+    },
+    {
+        "question": "In 2008, Usain Bolt set the world record for the 100 meters with one shoelace untied.",
+        "correct_answer": "True",
+    },
+    {
+        "question": "ATP tennis hosted several tournaments on carpet court before being replaced to reduce injuries.",
+        "correct_answer": "True",
+    },
+    {
+        "question": "There are a total of 20 races in Formula One 2016 season.",
+        "correct_answer": "False",
+    }
+]
